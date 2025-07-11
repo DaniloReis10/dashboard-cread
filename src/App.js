fa34fefb5c4a21f4365d96cfa36191066fcac176
Danilo Vasconcelos <daniloreis@MacBook-Pro-de-Danilo.local>
2025-07-11 15:57:35 -0300

    Primeiro commit: projeto Dashboard CREAD inicial parte front

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MatriculasChart from './components/MatriculasChart';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Matrículas por Ano</h1>
+      <MatriculasChart />
+    </div>
+  );
+}
+
+export default App;
