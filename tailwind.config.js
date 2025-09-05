f4640b9603c78c3813cd3942d0156d11a0c2a76a
Luis André Oliveira Lessa <luislessa001@gmail.com>
2025-09-05 13:07:10 -0300

    feat: Configure Tailwind, dependencies, and global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        'ifce-primary': '#008037', // A standard green for IFCE
+        'ifce-dark': '#004d21',   // A darker shade
+      },
+    },
   },
   plugins: [],
-};
+}
+
